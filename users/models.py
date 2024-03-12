26d47322cef570477a3ce3d75cfcb89457f2497f
Norilka <NorilkaRus@gmail.com>
2024-03-12 22:36:57 +0300

    Start

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+NULLABLE = {'blank':  True, 'null': True}
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='E-mail')
+    telegram = models.CharField(max_length=20, verbose_name='Telegram')
+    phone = models.CharField(max_length=20, verbose_name='Номер телефона')
+    avatar = models.ImageField(upload_to='avatars/', verbose_name='Аватар', **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name='Страна', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
